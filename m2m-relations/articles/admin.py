c5dca7ed7d7670fa8358216ec5879ba2b71e0bb8
Xypmich <kvudev@gmail.com>
2022-09-05 11:20:36 +0300

    add formset

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -5,18 +5,23 @@ from django.forms import BaseInlineFormSet
 from .models import Article, Tag, Scope
 
 
-# class ScopeInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             var = form.cleaned_data
-#             if var:
-#                 raise ValidationError('Может быть только 1 основной тег!')
-#         return super().clean()
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main_count = 0
+        for form in self.forms:
+            var = form.cleaned_data
+            if var.get('is_main'):
+                is_main_count += 1
+        if is_main_count > 1:
+            raise ValidationError('Может быть только 1 основной тег!')
+        elif is_main_count < 1:
+            raise ValidationError('Должен быть хотя бы 1 основной тег!')
+        return super().clean()
 
 
 class ScopeInline(admin.TabularInline):
     model = Scope
-    # formset = ScopeInlineFormset
+    formset = ScopeInlineFormset
     extra = 1
 
 
